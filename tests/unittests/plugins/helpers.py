e9ee4823cb6fa19a20fb41a3c483a035650de7ba
yen3 <yen3rc@gmail.com>
2022-11-22 15:44:21 +0100

    Enable mypy

--- tests/unittests/plugins/helpers.py
+++ tests/unittests/plugins/helpers.py
@@ -3,6 +3,7 @@
 
 class MockDLTMessage(object):
     """Mock DLT message for dltlyse plugin testing"""
+
     def __init__(self, ecuid="MGHS", apid="SYS", ctid="JOUR", sid="958", payload="", tmsp=0.0, sec=0, msec=0, mcnt=0):
         self.ecuid = ecuid
         self.apid = apid
@@ -28,6 +29,7 @@ class MockDLTMessage(object):
 
 class MockStorageHeader(object):
     """Mock DLT storage header for plugin testing"""
+
     def __init__(self, msec=0, sec=0):
         self.microseconds = msec
         self.seconds = sec
